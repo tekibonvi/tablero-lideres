7f6d70f0eb9b103f213e96befc1e84bf74c71830
Ezequias Bonvissuto <95387445+tekibonvi@users.noreply.github.com>
2025-07-11 12:32:12 -0300

    Create data.py

---
+++ modules/data/data.py
@@ -0,0 +1,32 @@
+
+# Estos id se usan para poder filtrar los datos de los agentes que pertenecen al CSQ 0810
+# Se usan en el script de llamadas.py , tiempos.py
+# Se pueden agregar o quitar agentes de esta lista según sea necesario
+
+id_0810 = ["aaudisio","angonzalez","aparisi","avera","aycaceres","cfriss","cmartino","dliendo","eorpianesi","fbustamante","fredondo",
+"ialvarez","icefarelli","kcarilaf","lfenoglio","lgaletto","lgiacconi","lguatelli","lillia","lmedina","lre","mguerrero","mibravo","mmartirena",
+"mpieretti","mstronatti","pperez","ptrujillo","rgatica","sdominech","smatich","spoma","tvillanueva"	,"voviedo","vperaffan","ybadolato","jloaiza","jsarria","everdi","narodriguez","calmiron"]
+
+
+id_SF_0810  = ["FREDONDO","LMEDINA","MDASSO","RGATICA","LILLIA","SPOMA","SMATICH","MIBRAVO","MMARTIRE","MSTRONAT","MPIERETT","CFRISS","LFENOGLI","AMALIER","KCARILAF","EORPIANE","ICEFAREL","PPEREZ",
+"LGIACCON","LGUATELL","IALVAREZ","TVILLANU","APARISI","NRUIZ","NJUAREZ","DLIENDO","VPERAFFA","PTRUJILL","YBADOLAT","LGALETTO","CMARTINO",
+"LRE","MGUERRER","IMANDELL","AVERA","ANGONZAL","FBUSTAMA","SDOMINEC","AYCACERE", "jsarria", "everdi","JLOAIZA","aaudisio","VOVIEDO","CALMIRON","NARODRIG"]
+
+
+TYPES = ['BOT', 'Correo Electronico', 'Web', 'Whatsapp']
+
+
+Ospedea = ["LGUATELL","MPIERETT","MSTRONAT"]
+
+
+TRASLADOS = ["MPISTONO", "RROMERO","MAGIMENE","JODRIOZO","GKLUWAK","EPEREYRA"]
+
+OSPEDEA_2 = [
+"Mariel Stronatti",
+"Laura Guatlli",
+"Lucia Fenoglio",
+"Matias Pieretti",
+"Sofia Rodriguez Matich",
+"Antonela Vera"]
+
+id_Sf_CRR = ["ABROMERO","MAVEGA","GPRINA","BVAQUIE","CDIAZDEA","VGIUSTI","FENRRICO","JNAVARRO","JRODRIGU","MRIOS","FMOLINA","LGIACONE"]
